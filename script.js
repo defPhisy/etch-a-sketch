57e72d51f1656c1acaac3e91bb99b0d0f2a80fcf
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-08-14 16:12:33 +0200

    ✨ add color features

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const container = document.querySelector(".container");
-
-const resetBtn = document.querySelector(".reset");
+const resetBtn = document.querySelector(".reset-btn");
 resetBtn.addEventListener("click", resetCanvasGrid);
 
 function getGridLength() {
@@ -12,7 +10,7 @@ function getGridLength() {
 function getUserInput() {
   return parseInt(
     prompt(
-      "What canvas length you want to choose? \n You can pick between 1 and 100!"
+      "What canvas length do you want to choose? \n You can pick between 1 and 100!"
     )
   );
 }
@@ -28,9 +26,10 @@ function checkUserInput(input) {
   return input;
 }
 
-function createCanvasGrid() {
-  const squaresLength = getGridLength();
-  const canvasWidth = container.clientWidth;
+function createCanvasGrid(length) {
+  const container = document.querySelector(".container");
+  const squaresLength = length || getGridLength();
+  const canvasWidth = container.clientWidth - 32; //32 are the 2rem padding in styles.css
   const squareLength = canvasWidth / squaresLength;
 
   for (let i = 0; i < squaresLength; i++) {
@@ -43,16 +42,17 @@ function createCanvasGrid() {
       row.style.width = `${squareLength}px`;
       row.style.height = `${squareLength}px`;
       row.classList = `row${i + 1}`;
-      row.style.backgroundColor = "red";
-      row.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "cyan";
-      });
+      row.style.backgroundColor = "hsl(0, 0%, 100%)";
+      row.style.opacity = 1;
+      row.addEventListener("mouseover", selectedMods);
+
       column.appendChild(row);
     }
   }
 }
 
 function deleteCanvasGrid() {
+  const container = document.querySelector(".container");
   const columns = container.querySelectorAll("div");
   for (let i = 0; i < columns.length; i++) {
     columns[i].remove();
@@ -64,4 +64,43 @@ function resetCanvasGrid() {
   createCanvasGrid();
 }
 
-createCanvasGrid();
+function selectedMods(e) {
+  const shadesIsChecked = document.querySelector("#shades-of").checked;
+
+  if (shadesIsChecked) {
+    modShadesOf(e);
+  } else {
+    colorPicker(e);
+    e.target.style.opacity = 1;
+  }
+}
+
+function colorPicker(e) {
+  const colorPicker = document.querySelector("#color-picker");
+  const color = colorPicker.options[colorPicker.selectedIndex].text;
+
+  if (color === "random color") {
+    e.target.style.backgroundColor = randomColor();
+  } else {
+    e.target.style.backgroundColor = color;
+  }
+}
+
+function randomColor() {
+  const red = randomRgbValue();
+  const green = randomRgbValue();
+  const blue = randomRgbValue();
+  const alpha = 1;
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+}
+
+function randomRgbValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function modShadesOf(e) {
+  let parsedOpacity = parseFloat(e.target.style.opacity);
+  e.target.style.opacity = parsedOpacity - 0.1;
+}
+
+createCanvasGrid(16);
